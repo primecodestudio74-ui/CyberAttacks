929292817c9d2353720a836b36c92cca90530eea
aniketbarai <aniketbarai677@gmail.com>
2026-03-30 12:32:37 +0530

    backend 1.1

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = 5000;
 
-// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Backend running...");
-});
+// API Routes
+app.use('/api/auth', authRoutes);
 
-// Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
